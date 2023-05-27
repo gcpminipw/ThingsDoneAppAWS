1f80872a0e74ed365a2a2158e2a135ee956402c6
mstaczek <matemmatem2000@gmail.com>
2023-05-27 08:57:53 +0200

    add adding and displaying items js code

--- js/ride.js
+++ js/ride.js
@@ -104,4 +104,94 @@ WildRydes.map = WildRydes.map || {};
     function displayUpdate(text) {
         $('#updates').append($('<li>' + text + '</li>'));
     }
+
+    // // functions related to thingsdoneapp
+
+    // function to display items in the list from json
+    // the html list has the id "items"
+    function displayItems(json) {
+        // get the list of items
+        var items = document.getElementById("items");
+        // clear the list
+        items.innerHTML = "";
+        // for each item in the json
+        for (var i = 0; i < json.length; i++) {
+            // create a new list item with the item id as the id
+            var li = document.createElement("li");
+            li.id = json[i].id;
+            // add the item to the list
+            items.appendChild(li);
+            // add the title, comment and score to the list item
+            li.innerHTML = json[i].title + " " + json[i].comment + " " + json[i].score;
+            // add a delete button to the list item
+            var deleteButton = document.createElement("button");
+            deleteButton.innerHTML = "Delete";
+            deleteButton.onclick = function() {
+                deleteItem(li);
+            };
+            li.appendChild(deleteButton);
+            // add an edit button to the list item
+            var editButton = document.createElement("button");
+            editButton.innerHTML = "Edit";
+            editButton.onclick = function() {
+                makeItemEditable(li);
+            };
+            li.appendChild(editButton);
+        }
+    }
+
+    // function to get the list of items from the server
+    function getItems() {
+        // use jquery ajax to get the items from API endpoint _config.api.invokeUrl + '/getallitems',
+        // using the authToken
+        $.ajax({
+            method: 'GET',
+            url: _config.api.invokeUrl + '/getallitems',
+            headers: {
+                Authorization: authToken
+            },
+            contentType: 'application/json',
+            success: function(data) {
+                // when the data is returned, display the items
+                displayItems(data);
+            }
+        });
+    }
+    // function to add an item to the list
+    function addItem() {
+        // get the title, comment and score from the html form
+        var title = document.getElementById("title").value;
+        var comment = document.getElementById("comment").value;
+        var score = document.getElementById("score").value;
+        // use jquery ajax to post the item to API endpoint _config.api.invokeUrl + '/additem',
+        // using the authToken
+        console.log('adding item');
+        //print item to be added
+        console.log(title);
+        console.log(comment);
+        console.log(score);
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/additem',
+            headers: {
+                Authorization: authToken
+            },
+            data: JSON.stringify({
+                title: title,
+                comment: comment,
+                score: score
+            }),
+            contentType: 'application/json',
+            success: function(data) {
+                //log the data
+                console.log('success in adding new data');
+                console.log(data);
+                // when the data is returned, display the items
+                getItems();
+            }
+        });
+    }
+
+
+
 }(jQuery));
