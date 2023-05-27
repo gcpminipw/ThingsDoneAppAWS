962e41390305eaf5ab694fa3912a7aa9fbe69968
mstaczek <matemmatem2000@gmail.com>
2023-05-27 07:38:12 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ThingsDoneAppAWS
+
 UI for a simple Cloud Computing project in AWS.
+
+Source: https://aws.amazon.com/getting-started/hands-on/build-serverless-web-app-lambda-apigateway-s3-dynamodb-cognito/
---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_vjtok7ZZb', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3i1d0ovo8kl0atq80u1q6ke8rm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: ' us-east-1 ' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://gpnlw5jke4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
